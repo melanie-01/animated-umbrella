558718b6e9aebb5309c335f092846fa72adb403c
Braedon Hume (1000116603) <HUMEBM1@student.op.ac.nz>
2025-05-06 13:48:43 +1200

    Adding Braedon Code

--- Program.cs
+++ Program.cs
@@ -5,8 +5,24 @@
         static void Main(string[] args)
         {
             //Braedon Write
-
-
+            Console.WriteLine("Hi my name is Braedon");
+            Console.Write("Please enter the starting letter: ");
+            string temp = Console.ReadLine();
+            Console.WriteLine("");
+            char start = Convert.ToChar(temp);
+            Console.Write("Please enter the stopping letter: ");
+            temp = Console.ReadLine();
+            Console.WriteLine("");
+            char stop = Convert.ToChar(temp);
+            for (int row = start; row <= stop; row++)
+            {
+                for (char col = start; col <= row; col++)
+                {
+                    Console.Write(col);
+                }
+                Console.WriteLine();
+            }
+            Console.ReadLine();
             //Melanie
 
             //Albert
